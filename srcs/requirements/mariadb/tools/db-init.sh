9c099564d04f7d55e9c0317ab054b53848934e99
SalahEddine EL HIMEUR <salaheddine.elhimeur@edu.uiz.ac.ma>
2026-07-26 01:15:55 +0100

    done

--- srcs/requirements/mariadb/tools/db-init.sh
+++ srcs/requirements/mariadb/tools/db-init.sh
@@ -1,43 +1,33 @@
 #!/bin/bash
-set -e
 
-MYSQL_PASSWORD="password"
-MYSQL_ROOT_PASSWORD="password2"
-MYSQL_DATABASE="table2"
-MYSQL_USER="test3"
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+mkdir -p /var/run/mysqld
 
-mkdir -p /run/mysqld
-chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB..."
-
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mysqld_safe --skip-networking &
 
-    mysqld_safe --user=mysql --skip-networking &
-    TEMP_PID=$!
 
-    until mysqladmin --socket=/run/mysqld/mysqld.sock ping --silent; do
+    until mysqladmin ping --silent; do 
         sleep 1
     done
 
-    mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
+    mysql -u root << EOF
+        CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`; 
+        CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+        GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+        FLUSH PRIVILEGES;
 EOF
 
-    echo "Databases after initialization:"
-    mysql -u root -e "SHOW DATABASES;"
+    
+    mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
-    mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
-    wait "$TEMP_PID"
-
-else
-    echo "MariaDB already initialized."
 fi
 
-exec mysqld --user=mysql
+exec mysqld
