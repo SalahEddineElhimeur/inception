aed9e1433070fae5bff2704fa8c2bdf7e59dacc3
SalahEddineElHimeur <salaheddine.elhimeur@edu.uiz.ac.ma>
2026-08-01 19:40:40 +0100

    added nginx v2

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -25,7 +25,7 @@ wp config create \
 
 
 wp core install \
-    --url=https://"$DOMAIN_NAME:8080" \
+    --url=https://"$DOMAIN_NAME" \
     --title="$WP_TITLE" \
     --admin_user="$WP_ADMIN_USER" \
     --admin_password="$WP_ADMIN_PASSWORD" \
